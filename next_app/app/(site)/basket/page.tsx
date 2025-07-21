2450ab25641605ba8cb9aeea6b74b49b907a0024
Drobysz <alexanderdrobyschevski@gmail.com>
2025-07-21 23:03:42 +0200

    * Replace useEffect data fetching with direct server fetching via getSessionData

--- next_app/app/(site)/basket/page.tsx
+++ next_app/app/(site)/basket/page.tsx
@@ -1,8 +1,30 @@
-// View
-import { BasketView } from "./BasketView";
+// Layout Styles
+import styles from './layout/BasketGridLayout.module.scss'
+
+// Layout components
+import { Header } from "./layout/Header";
+import { SideBar } from "./layout/SideBar";
+import { BasketSpace } from "./layout/BasketSpace";
+
+// Components
+import { FullScreenSpin } from '@/components';
+
+
+// Fetch functions
+import { getBasketContent } from '@/helpers/basketRequest';
+import { getBeatImages } from '@/helpers/s3LibRequests';
+
+// Props
+import { BeatUrl } from '@/interfaces/s3ElementData.interface';
+import { Beat } from '@/interfaces/Products.interface';
+import { UserSession } from '@/interfaces/UserData.interface';
 
 // Meta data
 import metaData from '@/metadata/metadata.json';
+
+
+// Session
+import { getSessionData } from '@/app/actions/sesssions';
 
 export async function generateMetadata(){
   return {
@@ -11,9 +33,25 @@ export async function generateMetadata(){
   };
 };
 
+export default async function BasketPage() {
+  const sessionData: UserSession | undefined = await getSessionData();
 
-export default function Page () {
+  if (sessionData === undefined) {
+    return <FullScreenSpin />
+  }
+
+  const basketBeats: Beat[] = await getBasketContent(sessionData.token!);
+  const beatImgs: BeatUrl[] = await getBeatImages();
+
+  if (!basketBeats || !beatImgs) {
+    return <FullScreenSpin />;
+  }
+  
   return (
-    <BasketView />
+    <div className={styles.wrapper}>
+      <Header className={styles.header}/>
+      <SideBar basketBeats={basketBeats} className={styles.sidebar}/>
+      <BasketSpace beatImgs={beatImgs} basketBeats={basketBeats}  className={styles.main} />
+    </div>
   );
-};
+}
