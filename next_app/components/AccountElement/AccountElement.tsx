e78b85b33bc97305d2ff40af59d91835ab409955
Drobysz <alexanderdrobyschevski@gmail.com>
2025-07-22 13:23:49 +0200

    * Fix bug with menu icon not appearing at 790px nearby login button

--- next_app/components/AccountElement/AccountElement.tsx
+++ next_app/components/AccountElement/AccountElement.tsx
@@ -24,24 +24,34 @@ export const AccountElement = () => {
     const [ isHovered, setHover ] = useState(false);
 
     return (
-        sessionData !== undefined
-        ?
             <div className="flex gap-4">
-                <div 
-                    className="rounded-full h-[52px] bg-[#2B2A2A]/80 flex items-center cursor-pointer gap-3 text-pinkish hover:scale-98 hover:ring-2 hover:ring-link-blue hover:drop-shadow-link-blue hover:drop-shadow-[0px_0px_10px] hover:text-link-blue transition-all duration-300 max-[1040px]:bg-transparent"
-                    onClick={ ()=> setModalWindow(true) }
-                    onMouseEnter={ ()=> setHover(true) }
-                    onMouseLeave={ ()=> setHover(false) }
-                >
-                    <Image 
-                        src={ isHovered ? '/user-logo_hovered.png' : '/user_logo.png' }
-                        width={52}
-                        height={52}
-                        alt="user"
-                    />
+                {sessionData !== undefined
+                ?
+                    <div 
+                        className="rounded-full h-[52px] bg-[#2B2A2A]/80 flex items-center cursor-pointer gap-3 text-pinkish hover:scale-98 hover:ring-2 hover:ring-link-blue hover:drop-shadow-link-blue hover:drop-shadow-[0px_0px_10px] hover:text-link-blue transition-all duration-300 max-[1040px]:bg-transparent"
+                        onClick={ ()=> setModalWindow(true) }
+                        onMouseEnter={ ()=> setHover(true) }
+                        onMouseLeave={ ()=> setHover(false) }
+                    >
+                        <Image 
+                            src={ isHovered ? '/user-logo_hovered.png' : '/user_logo.png' }
+                            width={52}
+                            height={52}
+                            alt="user"
+                        />
 
-                    <h2 className={cn("text-xl mr-4 max-[1040px]:hidden", climate_crisis.className)}>{sessionData.name}</h2>
-                </div>
+                        <h2 className={cn("text-xl mr-4 max-[1040px]:hidden", climate_crisis.className)}>{sessionData.name}</h2>
+                    </div>
+                :
+                    <div className="flex gap-4">
+                        <CustomBtn
+                            color="blue"
+                            link="/login"
+                        >
+                            Login
+                        </CustomBtn>
+                    </div>
+                }
 
                 <Image 
                     src='/Menu.svg'
@@ -52,25 +62,5 @@ export const AccountElement = () => {
                     onClick={()=> setMenuWindow(true)}
                 />
             </div>
-            
-        :
-            <div className="flex gap-4">
-                <CustomBtn
-                    color="blue"
-                    link="/login"
-                >
-                    Login
-                </CustomBtn>
-
-                <Image 
-                    src='/Menu.svg'
-                    width={40}
-                    height={40}
-                    alt="menu"
-                    className="min-[790px]:hidden hover:scale-110 transition-all duration-500 active:scale-100"
-                    onClick={()=> setMenuWindow(true)}
-                />
-            </div>
-            
     )
 };
