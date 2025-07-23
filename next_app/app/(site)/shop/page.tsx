27e18457fb7ce05ceb61228a9a2f5e070a49599b
Drobysz <alexanderdrobyschevski@gmail.com>
2025-07-23 15:19:51 +0200

    * Delete ShopView * Replace useEffect data fetching with direct fetching in server component

--- next_app/app/(site)/shop/page.tsx
+++ next_app/app/(site)/shop/page.tsx
@@ -1,9 +1,20 @@
-// View
-import { ShopView } from './ShopView';
+// Page
+import { ProductsListPage } from "./layout/ProductsListPage";
+
+// Props
+import { Beat } from "@/interfaces/Products.interface";
+import { BeatUrl } from "@/interfaces/s3ElementData.interface";
+
+// Skeleton component/Hooks
+import { FullScreenSpin } from "@/components/index";
+import { Suspense } from "react";
+
+// Helpers
+import { getProducts } from "@/helpers/productsRequest";
+import { getBeatImages, getBeatFiles } from "@/helpers/s3LibRequests";
 
 // Meta data
 import metaData from '@/metadata/metadata.json';
-
 
 export async function generateMetadata(){
   return {
@@ -12,8 +23,14 @@ export async function generateMetadata(){
   };
 };
 
-export default function Page () {
-  return (
-    <ShopView />
-  );
+export async function ShopView () {
+    const beats:      Beat[]    = await getProducts();
+    const beatImages: BeatUrl[] = await getBeatImages();
+    const beatFiles:  BeatUrl[] = await getBeatFiles();
+
+    return (
+        <Suspense fallback={<FullScreenSpin />}>
+            <ProductsListPage beats={beats} beatImages={beatImages} beatFiles={beatFiles}/>
+        </Suspense>
+    );
 };
