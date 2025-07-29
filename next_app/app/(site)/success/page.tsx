44052cba1fda4145aabeb164e15c9e4b038d1f8b
Drobysz <alexanderdrobyschevski@gmail.com>
2025-07-29 13:40:46 +0200

    * Make page “/success” server one and adapt its code for it * Adapt code of page “/success” for cookies sessions

--- next_app/app/(site)/success/page.tsx
+++ next_app/app/(site)/success/page.tsx
@@ -1,17 +1,39 @@
-'use client'
-
 // Components
-import { FullScreenSpin } from "@/components";
-import { InventoryPage } from "./InventoryPage";
+import { FullScreenSpin } from "@/components"
+
+// Nav
+import { redirect } from "next/navigation";
+
+// Hooks
 import { Suspense } from "react";
 
+// Fetch function
+import { setProduct } from "@/helpers/productsRequest";
+import { getSessionData } from "@/app/actions/sesssions";
 
-export default function Page (){
-    
+// Props
+import { UserSession } from "@/interfaces/UserData.interface";
 
-    return(
-        <Suspense fallback={<FullScreenSpin />}>
-            <InventoryPage />
-        </Suspense>
+
+export default async function PaymentCheckingPage ({
+    searchParams
+}: {
+    searchParams: Promise<{ product_id: string }>
+}) {
+
+    const sessionData: UserSession | undefined = await getSessionData(); 
+    const { product_id } = await searchParams;
+
+    if ( sessionData === undefined || !sessionData?.token || !product_id ) {
+        return null;
+    }
+
+    const token = sessionData.token!
+
+    setProduct(token, Number(product_id));
+    redirect('/inventory');
+
+    return (
+        <Suspense fallback={<FullScreenSpin />} />
     );
-}
+};
