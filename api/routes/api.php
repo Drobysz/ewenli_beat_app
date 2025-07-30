766e8951ed9d346fb7fee566a0f8a2995c1ce52d
Drobysz <alexanderdrobyschevski@gmail.com>
2025-07-30 14:33:02 +0200

    * Replace CRUD operations with apiResources * Unify routes with prefixes * Unify routes with middleware methodes

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,69 +4,52 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Controllers
-use App\Http\Controllers\Api\BeatController;
-use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Api\InventoryController;
-use App\Http\Controllers\Api\BasketController;
-use App\Http\Controllers\Api\S3ImagesController;
-use App\Http\Controllers\Api\S3BeatsController;
-use App\Http\Controllers\Api\StripeController;
+use App\Http\Controllers\Api\{
+    BeatController,
+    UserController,
+    InventoryController,
+    BasketController,
+    S3ImagesController,
+    S3BeatsController,
+    StripeController
+};
 
 // CRUD actions for Beats
-Route::get('/beats', [BeatController::class, 'index']);
+Route::apiResource('beats', BeatController::class);
 
-Route::get('/beats/{id}', [BeatController::class, 'show']);
-
-Route::put('/beats', [BeatController::class, 'store']);
-
-Route::patch('/beats/{id}', [BeatController::class, 'update']);
-
-Route::delete('/beats/{id}', [BeatController::class, 'destroy']);
-
+// Category list route
 Route::get('/categories', [BeatController::class, 'categories']);
 
-// Index for Users
+// User list route
 Route::get('/users', [UserController::class, 'index']);
 
-// Authenfication (register/login)
+// Authenfication (register/login/logout) routes
 Route::post('/register', [UserController::class, 'register']);
+Route::post('/login',    [UserController::class, 'login']);
+Route::post('/logout',   [UserController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::post('/login', [UserController::class, 'login']);
-
-Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
-
-// Modify user's data
+// Modify user's data routes
 Route::patch('/change-password', [UserController::class, 'changePassword']);
-
-Route::patch('/change-email', [UserController::class, 'changeEmail']);
-
+Route::patch('/change-email',    [UserController::class, 'changeEmail']);
 Route::patch('/change-nickname', [UserController::class, 'changeNickname']);
 
-// Store, Destroy and Index for Inventory and Basket
-
-// Add to Inventory
-Route::post('/inventory/{id}', [InventoryController::class, 'store'])->middleware('auth:sanctum');
-
-// Show
-Route::get('/inventory', [InventoryController::class, 'index'])->middleware('auth:sanctum');
-
-// Add to Basket
-Route::post('/basket/{id}', [BasketController::class, 'store'])->middleware('auth:sanctum');
-
-// Delete from Basket
-Route::delete('/basket/{id}', [BasketController::class, 'destroy'])->middleware('auth:sanctum');
-
-// Show
-Route::get('/basket', [BasketController::class, 'index'])->middleware('auth:sanctum');
+// Store, Destroy and Index routes for Inventory and Basket
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('inventory', InventoryController::class)->only(['store', 'index']);
+    Route::apiResource('basket',    BasketController::class)   ->only(['index', 'store', 'destroy']);
+});
 
 
-// S3AWSControllers
+// S3 storage routes
+Route::prefix('lib')->group( function() {
+    Route::get('beat_imgs',      [S3ImagesController::class, 'index']);
+    Route::get('beat_mp3_files', [S3BeatsController::class,  'index']);
+});
 
-// Music cover storage accessing
-Route::get('/lib/beat_imgs', [S3ImagesController::class, 'index']);
-
-// Audio file storage accessing
-Route::get('/lib/beat_mp3_files', [S3BeatsController::class, 'index']);
-
-// Stripe Session
+// Stripe session route
 Route::post('/create-checkout-session', [StripeController::class, 'createCheckoutSession']);
+
+// Fallback for undefined routes
+Route::fallback(function () {
+    return response()->json(["message" => "Not found"], 404);
+});
