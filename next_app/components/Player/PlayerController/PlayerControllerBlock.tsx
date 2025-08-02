4391f2b4aa14abbb5c5e290425e7c44abed3139b
Drobysz <alexanderdrobyschevski@gmail.com>
2025-08-02 16:47:00 +0200

    * Delete commented code (track switchers) * Delete importing of function handleArrowClick

--- next_app/components/Player/PlayerController/PlayerControllerBlock.tsx
+++ next_app/components/Player/PlayerController/PlayerControllerBlock.tsx
@@ -5,17 +5,8 @@ import { PlayerProps } from './PlayerController.props';
 import Image from 'next/image';
 
 export const PlayerController = ({handleResumeClick, isPlaying}: PlayerProps)=> {
-
     return (
         <div className='flex justify-end'>
-            {/* <Image 
-                className='cursor-pointer'
-                src='/playerArrow.svg'
-                width={40}
-                height={40}
-                alt='arrow'
-                onClick={()=> handleArrowClick('-')}
-            /> */}
             <Image 
                 className='cursor-pointer'
                 src={ isPlaying ? '/stop.svg' : '/resume.svg' }
@@ -24,14 +15,6 @@ export const PlayerController = ({handleResumeClick, isPlaying}: PlayerProps)=> 
                 alt='player'
                 onClick={()=> handleResumeClick()}
             />
-            {/* <Image 
-                className='rotate-180 cursor-pointer transition-all duration-400'
-                src='/playerArrow.svg'
-                width={40}
-                height={40}
-                alt='arrow'
-                onClick={()=> handleArrowClick('+')}
-            /> */}
         </div>
     );
 };
