e7fa4e15b3bfb2a91c73f1864e51021f9ede386e
Drobysz <alexanderdrobyschevski@gmail.com>
2025-09-28 21:57:12 +0200

    * Make SearchBar execute instant search request
    - SearchBar will be executing search request once a change to text is made

--- next_app/components/SearchBar.tsx
+++ next_app/components/SearchBar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 // Hooks
-import { useContext, useState } from "react";
+import { useContext } from "react";
 
 // Context
 import { ShopContext } from "@/app/(site)/shop/context/shop.context";
@@ -9,14 +9,12 @@ import { ShopContext } from "@/app/(site)/shop/context/shop.context";
 export const SearchBar = ()=> {
     // Search bar request value
     const { setSearchBarRequest } = useContext(ShopContext);
-    // Search bar value
-    const [ searchBarValue, setSearchBarValue ] = useState('');
+
 
     return (
         <input 
             type="text"
-            onChange={(e)=> setSearchBarValue(e.target.value)}
-            onKeyDown={(e)=> e.key === 'Enter' && setSearchBarRequest(searchBarValue)}
+            onChange={(e)=> setSearchBarRequest(e.target.value)}
             className="min-w-[180px] w-[70%] h-14 max-[610px]:h-10 rounded-2xl bg-[#282727] bg-[url(/Search.svg)] max-[682px]:bg-size-[40px] max-[610px]:bg-size-[30px] bg-position-[center_left_0.25rem] bg-no-repeat text-white/80 text-center max-[646px]:text-sm max-[610px]:text-xs max-[690px]:pl-10 max-[610px]:pl-5 placeholder-[#6B6969]"
             placeholder="input the beat name"
         />
