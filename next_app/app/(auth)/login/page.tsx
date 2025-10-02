216ea6352b2380d7fa22600ce08877e4242e6763
Drobysz <alexanderdrobyschevski@gmail.com>
2025-10-02 19:04:57 +0200

    * Conceal the social media btns on auth pages
    - the social media btns will be temporarely concealed
      until authenfication via other services integration

--- next_app/app/(auth)/login/page.tsx
+++ next_app/app/(auth)/login/page.tsx
@@ -1,18 +1,9 @@
-// Components
 import Image from "next/image";
-import { SocialAuthBtn, BackBtn } from '@/components/index';
+import { BackBtn } from '@/components/index';
 import Link from "next/link";
-
-// Form 
 import { LogForm } from "./LogForm/LogForm"
-
-// Font
 import { fs163, avenir } from '@/fonts/fonts';
-
-// Deps
 import cn from 'classnames';
-
-// Meta data
 import metaData from '@/metadata/metadata.json';
 
 export async function generateMetadata(){
@@ -27,8 +18,7 @@ export default function LoginPage() {
         <div 
             className="h-fit w-full backdrop-blur-xl bg-link-blue/10 overflow-hidden pt-5 pb-4 px-5 rounded-3xl border-1 border-zinc-700"
         >
-            <BackBtn className="absolute top-3 left-4 w-[100px]"/>
-            
+            <BackBtn className="absolute top-3 left-4 w-[100px]"/>  
             <div className="w-full flex justify-center mb-6">
                 <Image 
                     width={90} 
@@ -38,20 +28,16 @@ export default function LoginPage() {
                     className="bg-transparent"
                 />
             </div>
-            
-            <div className="grid grid-cols-2 gap-3 mb-6">
+            {/* <div className="grid grid-cols-2 gap-3 mb-6">
                 <SocialAuthBtn btnType="google"/>
                 <SocialAuthBtn btnType="github"/>
             </div>
-
             <div className="grid grid-cols-[1fr_auto_1fr] items-center gap-3 mb-2">
                 <div className="h-[2px] w-full bg-gray-200/50"/>
                 <span className="text-zinc-400">OR</span>
                 <div className="h-[2px] w-full bg-gray-200/50"/>
-            </div>
-
+            </div> */}
             <LogForm />
-
             <p className={cn(avenir.className, "text-center text-md text-white")}>
                 If you are not registered yet, there is a 
                 <Link href={'/register'}>
