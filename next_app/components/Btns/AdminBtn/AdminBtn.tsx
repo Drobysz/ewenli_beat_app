2a75234f11b1b0746829461d0a94a7ef02e8f2b8
Drobysz <alexanderdrobyschevski@gmail.com>
2025-10-02 19:33:00 +0200

    * Fix bug with shifting
    - Allocate an expanded width for component AdminBtn
      due it to not shift container content

--- next_app/components/Btns/AdminBtn/AdminBtn.tsx
+++ next_app/components/Btns/AdminBtn/AdminBtn.tsx
@@ -14,10 +14,13 @@ export const AdminBtn = ()=> {
 	const isConcealed = sessionData != undefined && sessionData!.role == 'admin' ? false : true;
 
 	return (
-		<Link href="/adm_panel">
+		<Link 
+			href="/adm_panel"
+			className='w-[85px]'
+		>
 			<motion.button
 				className={cn(
-					'rounded-full h-[35px] w-[35px] bg-green-light overflow-x-hidden pl-2 mb-1.5',
+					'rounded-full bg-green-light overflow-x-hidden pl-2 mb-1.5',
 					'active:scale-95 active:opacity-70 active:border-2 active:border-white duration-300 transition-all',
 					{['hidden']: isConcealed}
 				)}
@@ -43,12 +46,10 @@ export const AdminBtn = ()=> {
 						alt='admin'
 					/>
 					<h3 
-						className={
-							cn(
-								inter.className,
-								'text-white text-[0.65rem] whitespace-nowrap transition-all'
-							)
-						}
+						className={cn(
+							inter.className,
+							'text-white text-[0.65rem] whitespace-nowrap transition-all'
+						)}
 					>
 						admin panel
 					</h3>
